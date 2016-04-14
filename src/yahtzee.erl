eb0c2c78a08d4a7aef91faf0c7dac747f13ef7b6
Brian H. Ward <bward@virtualhold.com>
2016-04-14 16:21:26 -0400

    twos/1 and top_section_line abstraction

--- src/yahtzee.erl
+++ src/yahtzee.erl
@@ -4,10 +4,19 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--export([ones/1,three_of_a_kind/1]).
+-export([ones/1,twos/1,three_of_a_kind/1]).
 
 ones(Roll) ->
-    lists:sum(lists:filter(fun(Die) -> Die == 1 end,Roll)).
+    top_section_line(1,Roll).
+
+twos(Roll) ->
+    top_section_line(2,Roll).
+
+top_section_line(Line,Roll) ->
+    lists:sum(lists:filter(fun(Die) ->
+				   Die == Line
+			   end,Roll)).
+
 
 three_of_a_kind(Roll) ->
     apply(fun three_of_a_kind/6,[lists:sum(Roll)|lists:sort(Roll)]).
@@ -24,6 +33,16 @@ three_of_a_kind(_Score,_A,_B,_C,_D,_E) ->
 -ifdef(EUNIT).
 
 %%% EUnit tests for internal/private functions
+top_section_line_test_() ->
+    [
+     {"score zero points for zero ones",
+      ?_assertEqual(0,top_section_line(1,[2,3,4,5,6]))},
+     {"score one point for a single one",
+      ?_assertEqual(1,top_section_line(1,[1,2,3,4,5]))},
+     {"score five for all ones",
+      ?_assertEqual(5,top_section_line(1,[1,1,1,1,1]))}
+    ].
+
 three_of_a_kind_test_() ->
     [
      {"pass out zero on no-match",
--- src/yahtzee_tests.erl
+++ src/yahtzee_tests.erl
@@ -4,7 +4,7 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--import(yahtzee,[ones/1,three_of_a_kind/1]).
+-import(yahtzee,[ones/1,twos/1,three_of_a_kind/1]).
 
 -ifdef(EUNIT).
 
@@ -16,6 +16,12 @@ ones_test_() ->
       ?_assertEqual(1,ones([1,2,3,4,5]))},
      {"score five for all ones",
       ?_assertEqual(5,ones([1,1,1,1,1]))}
+    ].
+
+twos_test_() ->
+    [
+     {"score zero points for zero ones",
+      ?_assertEqual(0,twos([1,3,4,5,6]))}
     ].
 
 three_of_a_kind_test_() ->
